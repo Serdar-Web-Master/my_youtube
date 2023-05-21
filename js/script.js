7b1f966f7a6e1b213d249a232a46704910678c2a
Serdar <serdarcholukov@yandex.com>
2023-05-21 15:55:13 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// const link = document.querySelector('.link');
+
+// setTimeout(() => {
+//    link.click()
+// }, 100);
+
+
+const link = {
+   
+}
+
+const a = document.createElement("a");
+
+console.log(a);
